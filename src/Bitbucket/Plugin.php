e37cf14480c0c0a0730b329c96a83721840bb01f
Benjamin Rush <bnjmnrsh@gmail.com>
2026-09-19 23:59:37 +0100

    refactor(provider): require registration context

--- src/Bitbucket/Plugin.php
+++ src/Bitbucket/Plugin.php
@@ -6,6 +6,7 @@ namespace RAN\Booster\Bitbucket;
 
 use RAN\RepositoryProvider\AuthenticatedWebhookDeliveryEvidenceReader;
 use RAN\RepositoryProvider\ProviderCredentialStore;
+use RAN\RepositoryProvider\ProviderRegistrationContext;
 use RAN\RepositoryProvider\ProviderRegistry;
 use RAN\RepositoryProvider\RepositoryProvider;
 
@@ -27,8 +28,10 @@ final class Plugin {
 			'bb',
 			static function (
 				ProviderCredentialStore $credentials,
-				AuthenticatedWebhookDeliveryEvidenceReader $deliveryEvidence
+				AuthenticatedWebhookDeliveryEvidenceReader $deliveryEvidence,
+				ProviderRegistrationContext $registrationContext
 			): RepositoryProvider {
+				unset( $registrationContext );
 				$api      = new BitbucketApiClient();
 				$loader   = new BitbucketCredentialLoader( $credentials );
 				$browser  = new BitbucketRepositoryBrowser( $loader, $api );
