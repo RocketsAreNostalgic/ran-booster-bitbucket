6081fa13401fd2e946567cd83f72e5861ca421f2
Benjamin Rush <bnjmnrsh@gmail.com>
2026-09-19 23:59:44 +0100

    test(provider): lock three-argument registration

--- tests/Plugin/CurrentCoreBoundaryTest.php
+++ tests/Plugin/CurrentCoreBoundaryTest.php
@@ -18,7 +18,9 @@ final class CurrentCoreBoundaryTest extends TestCase {
 		self::assertStringContainsString( '10 === RAN_BOOSTER_PROVIDER_API_VERSION', $plugin );
 		self::assertStringContainsString( '16 === RAN_BOOSTER_ADDON_API_VERSION', $plugin );
 		self::assertStringContainsString( 'AuthenticatedWebhookDeliveryEvidenceReader $deliveryEvidence', $plugin );
+		self::assertStringContainsString( 'ProviderRegistrationContext $registrationContext', $plugin );
 		self::assertStringNotContainsString( 'interface_exists( AuthenticatedWebhookDeliveryEvidenceReader::class )', $plugin );
+		self::assertStringNotContainsString( 'class_exists( ProviderRegistrationContext::class )', $plugin );
 	}
 
 	public function testRepositoryTestsUseOnlyTheCertifiedCoreProductionAutoloader(): void {
