326473823910635b39b52760207f74dbc009a6bd
Benjamin Rush <bnjmnrsh@gmail.com>
2026-09-21 11:53:50 +0100

    test: load certified Core runtime dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/fixtures/certified-core-checkout.php';
 
-$coreRoot     = ran_booster_bitbucket_certified_core_root();
-$coreAutoload = $coreRoot . '/autoload.php';
+$coreRoot           = ran_booster_bitbucket_certified_core_root();
+$coreVendorAutoload = getenv( 'RAN_BOOSTER_CORE_VENDOR_AUTOLOAD' );
+$coreAutoload       = $coreRoot . '/autoload.php';
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
+if ( false !== $coreVendorAutoload && '' !== $coreVendorAutoload ) {
+	require $coreVendorAutoload;
+}
 require $coreAutoload;
 require dirname( __DIR__ ) . '/autoload.php';
 
